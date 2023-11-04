668d49b6b88ba1a5b6f83de606d60188799f52e1
Joshua Omolegan <joshuaomolegan@gmail.com>
2023-11-04 17:26:56 +0000

    Boilerplate code for recieving HTML file via HTTP POST

--- src/app.py
+++ src/app.py
@@ -1,10 +1,34 @@
-from flask import Flask
+from flask import Flask, flash, request, redirect, url_for
+from html_modifier import html_accessibility
+import sys
 
 app = Flask(__name__)
+app.debug = True
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+HTTP_EMPTY_RESPONSE = 200
+HTTP_BAD_REQUEST = 400
+
+def is_html(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() == "html"
+
+@app.route("/", methods=['GET', 'POST'])
+def get_html():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            print('No file found', file=sys.stderr)
+            return "", HTTP_BAD_REQUEST
+        file = request.files['file']
+        if file.filename == '':
+            print('No selected file', file=sys.stderr)
+            return "", HTTP_BAD_REQUEST
+
+        if not is_html(file.filename):
+            print("Uploaded file is not a HTML file", file=sys.stderr)
+            return "", HTTP_BAD_REQUEST
+        
+        return html_accessibility(file)
+    
+    return "", HTTP_EMPTY_RESPONSE
 
 if __name__ == "__main__":
   app.run()
