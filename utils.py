f91673e80b27bd31908e4dc8b3a931e54219d1f3
abu <abu@joinzoe.com>
2023-11-04 21:38:51 +0000

    fix debug issue

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 from cache_to_disk import cache_to_disk
+import os
 
-DEBUG = True
+DEBUG = bool(os.environ.get("DEBUG", False))
 
 # I'm doing this to conserve our API credits for now:
 def debug_picklify(function):
