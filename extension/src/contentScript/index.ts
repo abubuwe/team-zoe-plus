06cec6e5aad7837fca9a7bf4ba4f4e6122abda9d
abu <abu@joinzoe.com>
2023-11-04 20:41:24 +0000

    add some stuff for railway

--- extension/src/contentScript/index.ts
+++ extension/src/contentScript/index.ts
@@ -1,1 +1,21 @@
 console.info('contentScript is running')
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    console.log(sender.tab ?
+        "from a content script:" + sender.tab.url :
+        "from the extension");
+        
+    if (request.type === 'COUNT') {
+      console.log('contentScript has received a message from popup, and count is ', request?.count)
+    }
+  
+    if (request.type === 'HTML') {
+      console.log('contentScript has received a message from content, and html is ', request?.html)
+    }
+
+    else {
+      console.log('contentScript has received a message and it is ', request)
+    }
+  })
+  
+  
