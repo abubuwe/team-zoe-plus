1265aae1d637ed3b22e7831a8a463d970c3956a1
TheBritishSabrina <sabrina.evans997@hotmail.co.uk>
2023-11-04 21:37:16 +0000

    Add contrast handler

---
+++ contrast_editor.py
@@ -0,0 +1,37 @@
+from colorutils import Color
+from lxml import etree
+from html_modifier import get_elem_from_path
+
+class ContrastEditor():
+    def __init__(self, dom: any):
+        self._dom = dom
+        self._handlers = {
+            # TODO: Find contrast error type
+        }
+
+    def _handle_contrast_error(self, details: dict):
+        xpaths = details["contrast"]["items"]["contrast"]["xpaths"]
+
+        contrast_data = details["contrast"]["items"]["contrast"]["contrastdata"]
+
+        changes_dict = {}
+
+        for xpath, data in zip(xpaths, contrast_data):
+            background_colour = data[2]
+            colour_obj = Color(background_colour)
+            
+            opp_colour_obj = colour_obj.commplementary()
+            opp_colour = opp_colour_obj.hex
+
+            elem = get_elem_from_path(xpath, self._dom)
+            elem.set("style", f"colour: {opp_colour}")
+
+            changes_dict[xpath] = etree.tostring(elem).decode()
+
+        return etree.tostring(self._dom).decode(), changes_dict
+    
+    def handle_contrast_error(self, error_type: str, details: dict):
+        if error_type not in self._handlers:
+            raise RuntimeError(f"Handler for contrast error: {error_type} not registered")
+        
+        self._handlers[error_type]
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from html_modifier import parse_html
 from flask import Flask, request
 
 from accessibility_editor import AccessibilityEditor
+from contrast_editor import ContrastEditor
 from utils import debug_picklify
 from dotenv import load_dotenv
 
@@ -32,8 +33,13 @@ def get_html():
 
         dom = parse_html(html_string)
         accessibility_editor = AccessibilityEditor(dom)
+        contrast_editor = ContrastEditor(dom)
         
-        return process_analysis(query_accessibility_errors(ACCESSIBILITY_API_URL), accessibility_editor)
+        return process_analysis(
+           query_accessibility_errors(ACCESSIBILITY_API_URL), 
+           accessibility_editor, 
+           contrast_editor
+         )
     
     return "", HTTP_EMPTY_RESPONSE
 
@@ -58,23 +64,40 @@ def query_accessibility_errors(website: str):
 
   return response.json()
 
-def process_analysis(results: dict, error_handler: AccessibilityEditor):
+def process_analysis(
+      results: dict, 
+      accessibility_editor: AccessibilityEditor,
+      contrast_editor: ContrastEditor
+   ):
    print(f"Visual analysis URL: {results['statistics']['waveurl']}")
    print(f"Total element count: {results['statistics']['totalelements']}")
 
-   errors, alerts = results['categories']['error']['items'], results['categories']['alert']['items']
+   errors = results['categories']['error']['items']
+   alerts = results['categories']['alert']['items']
+   contrast_errors = results['categories']['contrast']['items']
 
-   with tqdm(total = len(errors)) as pbar:
+   with tqdm(total = len(errors) + len(contrast_errors)) as pbar:
       for error_type, error in errors.items():
          pbar.set_description(f"Patching {error_type}...")
          
          try:
-            error_handler.handle_error(error_type, error)
+            accessibility_editor.handle_accessibility_error(error_type, error)
          except RuntimeError:
             # TODO: Remove this!
             pass
          
          pbar.update()
+
+      for contrast_type, contrast_error in contrast_errors.items():
+         pbar.set_description(f"Fixing contrast {contrast_type}...")
+         
+         try:
+            contrast_editor.handle_contrast_error(contrast_type, contrast_error)
+         except RuntimeError:
+            # TODO: Remove this!
+            pass
+         
+         pbar.update()      
    
    # TODO: Handle alerts!
 
